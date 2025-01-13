5ea551f34720ff0ef8e76d1f96f73c38a50fba42
odilo <odilomurindahabi@gmail.com>
2025-01-13 16:19:07 +0200

    add start script in package json

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+
+// Load environment variables
+dotenv.config();
+
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+	res.send("API is running...");
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
