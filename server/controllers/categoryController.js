07ba308e2a7b86e1a5a50c8cccd64f5d7dc82016
odilo <odilomurindahabi@gmail.com>
2025-01-15 11:15:11 +0200

    reactor category model

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require("express-async-handler");
-const Category = require("../models/categoryModel");
+const Category = require("../models/CategoryModel");
 
 // Create a new category
 const createCategory = asyncHandler(async (req, res) => {
