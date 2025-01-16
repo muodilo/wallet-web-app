759c6b702c472d307f65d9c0c0d98df982f02ccc
odilo <odilomurindahabi@gmail.com>
2025-01-16 11:49:26 +0200

    create landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,22 @@
-export default function App() {
-	return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+	return (
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+			</Routes>
+			<Footer />
+			<ToastContainer />
+			
+		</Router>
+	);
 }
+
+export default App;
